d4df83469bdd4325be570f58e2f12801f6d0bd0d
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2024-05-20 11:47:29 +0300

    close google script
    close google script

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import UserRoutes from './components/Routes/UserRoutes.jsx';
-import { Helmet } from 'react-helmet';
+// import { Helmet } from 'react-helmet';
 import ScrollToTopOnRouteChange from './components/helpers/ScrollToTopOnRouteChange';
 import ScrollToTopButton from './components/Shared/ScrollToTopButton';
 import SchemaOrg from 'components/helpers/SchemaOrg';
 
 function App() {
-  const googleAnalyticsScript = `
-    window.dataLayer = window.dataLayer || []; 
-    function gtag(){dataLayer.push(arguments);} 
-    gtag('js', new Date()); 
-    gtag('config', 'G-QS0LELBS8K');
-  `;
+  // const googleAnalyticsScript = `
+  //   window.dataLayer = window.dataLayer || [];
+  //   function gtag(){dataLayer.push(arguments);}
+  //   gtag('js', new Date());
+  //   gtag('config', 'G-QS0LELBS8K');
+  // `;
 
   return (
     <>
-      <Helmet>
+      {/* <Helmet>
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-QS0LELBS8K"
         />
         <script>{googleAnalyticsScript}</script>
-      </Helmet>
+      </Helmet> */}
       <SchemaOrg />
       <div
         style={{
